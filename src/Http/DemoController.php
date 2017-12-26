6ed45e00a3cd6cd14ea2e36d0ab0b984c813d2dc
Ali <totaldesign.ir@gmail.com>
2017-12-26 09:36:47 +0330

    First commit

---
+++ src/Http/DemoController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Websanova\Demo\Http;
+
+use Illuminate\Routing\Controller as BaseController;
+
+class DemoController extends BaseController
+{
+    public function index()
+    {
+        return \Demo::hello() . ' from controller.';
+    }
+}
